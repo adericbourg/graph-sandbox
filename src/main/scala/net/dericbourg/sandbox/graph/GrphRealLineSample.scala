9f85a04ebc31f9d8620cacbe40ad8f7cd4b1d58f
Alban Dericbourg <alban@dericbourg.net>
2018-01-18 11:49:50 +0100

    Handle directed lines

--- src/main/scala/net/dericbourg/sandbox/graph/GrphRealLineSample.scala
+++ src/main/scala/net/dericbourg/sandbox/graph/GrphRealLineSample.scala
@@ -7,15 +7,7 @@ import grph.path.ArrayListPath
 
 import scala.collection.mutable.ListBuffer
 
-/*
- FIXME: Does not handle that kind of line
- Eg: https://www.ratp.fr/plans-lignes/metro/7b
 
-         X -> X
-         ^    |
-         |    v
- X - X - X <- X
- */
 object GrphRealLineSample extends App {
 
   val g: Grph = new InMemoryGrph()
@@ -24,12 +16,9 @@ object GrphRealLineSample extends App {
   Station.getAll.foreach(station => g.addVertex(station.id))
 
   // Create connections from line maps
-  val fullMap: Seq[Seq[Station]] = Line1.LineMap ++ Line2.LineMap
-  fullMap.foreach { branch: Seq[Station] =>
-    branch.sliding(2).foreach {
-      case from :: to :: Nil => g.addSimpleEdge(from.id, to.id, false)
-      case other             => println(s"Panic: $other")
-    }
+  val fullMap: Seq[Connection] = Line1.LineMap ++ Line2.LineMap ++ Line3.LineMap
+  fullMap.foreach { connection =>
+    g.addSimpleEdge(connection.from.id, connection.to.id, connection.directed)
   }
 
   // Create line connections
@@ -37,13 +26,18 @@ object GrphRealLineSample extends App {
     g.addSimpleEdge(from.id, to.id, false)
   }
 
-  // Path from 1.1 to 2.7
-  val algorithm: DijkstraAlgorithm = new DijkstraAlgorithm(g.getVertexColorProperty)
-  val pathsFrom0: SearchResult = algorithm.compute(g, Line1.Station11.id)
-  val arrayListPath: ArrayListPath = pathsFrom0.computePathTo(Line2.Station27.id)
-  for (vertex <- arrayListPath.toVertexArray) {
-    println(vertex)
+  def getPath(from: Station, to: Station): Unit = {
+    println(s"From ${from.name} to ${to.name}")
+    val algorithm: DijkstraAlgorithm = new DijkstraAlgorithm(g.getVertexColorProperty)
+    val pathsFrom0: SearchResult = algorithm.compute(g, from.id)
+    val arrayListPath: ArrayListPath = pathsFrom0.computePathTo(to.id)
+    for (vertex <- arrayListPath.toVertexArray) {
+      println(vertex)
+    }
   }
+
+  getPath(Line1.Station11, Line2.Station27)
+  getPath(Line3.Station33, Line3.Station31)
 }
 
 case class Line(name: String, connections: Seq[Seq[Station]])
@@ -66,6 +60,9 @@ object Station {
 
 
 object Line1 {
+
+  import Connections._
+
   /*
                           to line 2
                          /
@@ -85,14 +82,17 @@ object Line1 {
   val Station18 = Station(18, "1.8")
   val Station19 = Station(19, "1.9")
 
-  val LineMap = Seq(
+  val LineMap: Seq[Connection] = Seq(
     Seq(Station11, Station12, Station13, Station14),
     Seq(Station14, Station15, Station16),
     Seq(Station14, Station17, Station18, Station19)
-  )
+  ).asConnections()
 }
 
 object Line2 {
+
+  import Connections._
+
   /*
    2.1 - 2.2 - 2.3 - 2.4 - 2.5 - 2.6 - 2.7 - 2.8 - 2.9
                              \
@@ -108,13 +108,49 @@ object Line2 {
   val Station28 = Station(28, "2.8")
   val Station29 = Station(29, "2.9")
 
-  val LineMap = Seq(
+  val LineMap: Seq[Connection] = Seq(
     Seq(Station21, Station22, Station23, Station24, Station25, Station26, Station27, Station28, Station29)
-  )
+  ).asConnections()
 }
+
+object Line3 {
+
+  import Connections._
+
+  /*
+     3.1 - 3.2 -> 3.3 -> 3.4
+            ^             |
+            |             |
+            ---------------
+   */
+
+  val Station31 = Station(31, "3.1")
+  val Station32 = Station(32, "3.2")
+  val Station33 = Station(33, "3.3")
+  val Station34 = Station(34, "3.4")
+
+  val LineMap: Seq[Connection] = Seq(
+    Seq(Station31, Station32),
+    Seq(Station32, Station31),
+    Seq(Station32, Station33, Station34, Station32),
+  ).asConnections(directed = true)
+}
+
+case class Connection(from: Station, to: Station, directed: Boolean = false)
 
 object Connections {
   val Values = Seq(
     (Line1.Station15, Line2.Station25)
   )
+
+  implicit class ConnectionBuilder(lineSequence: Seq[Seq[Station]]) {
+    def asConnections(directed: Boolean = false): Seq[Connection] = {
+      lineSequence.flatMap { branch: Seq[Station] =>
+        branch.sliding(2).map {
+          case from :: to :: Nil => Connection(from, to, directed)
+        }
+      }
+    }
+  }
+
 }
